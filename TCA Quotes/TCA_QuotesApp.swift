d0ac72ebc583af16947979cf1b8956e8b9ad9180
Yehor Podolskyi <66732525+yehorPo@users.noreply.github.com>
2023-06-12 14:25:32 +0300

    Initial Commit

---
+++ TCA Quotes/TCA_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_QuotesApp.swift
+//  TCA Quotes
+//
+//  Created by Yehor Podolskyi on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TCA_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
